9bef2d1826777b6bade94aedc4e0a0a6ef7341bb
catskhi <catskhi@proton.me>
2025-02-21 21:39:08 -0300

    refactor: Refactored layout, adding metadata and custom font to project

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,26 @@
+import { Metadata} from "next";
+import localFont from 'next/font/local';
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title:"Hivemind",
+  description: "Secure communication"
+}
+
+const webplusFont = localFont({
+  src: '../public/fonts/Ac437_ToshibaSat_8x14.ttf'
+})
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html>
-      <body suppressHydrationWarning={true}>
+    <html className={webplusFont.className}>
+      <head>
+      </head>
+      <body className="dark" suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
