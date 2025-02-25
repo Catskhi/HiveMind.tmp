c546ad2cbd63389f0ce272676adcdedb91c96573
catskhi <catskhi@proton.me>
2025-02-24 22:45:55 -0300

    feat: Added fonts script

---
+++ frontend/app/styles/font.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const gohuFont = localFont({
+  src: '../../public/fonts/GohuFont14NerdFontMono-Regular.ttf'
+})
+
+export const toshibaSatFont = localFont({
+  src: '../../public/fonts/Ac437_ToshibaSat_8x14.ttf'
+})
+
+export const webplusFont = localFont({
+  src: '../../public/fonts/WebPlus_HP_100LX_6x8.woff'
+})
