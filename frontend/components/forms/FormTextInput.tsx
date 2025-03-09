2224a76ceda8de0c8ad11351060ba22cb917044f
catskhi <catskhi@proton.me>
2025-03-08 23:49:31 -0300

    refactor: Added more custom properties to FormTextInput

--- frontend/components/forms/FormTextInput.tsx
+++ frontend/components/forms/FormTextInput.tsx
@@ -4,14 +4,19 @@ interface Props {
     required?: boolean,
     className?: string,
     placeholder?: string
+    value?: string
+    onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function FormTextInput({type, required, className, placeholder, onChange}: Props) {
+export default function FormTextInput({type, required, className, placeholder, onChange, onKeyDown, value}: Props) {
     return (
-        <input type={type ? type : "text"} required={required}
+        <input 
+        value={value}
+        type={type ? type : "text"} required={required}
+        onKeyDown={onKeyDown}
         className={className + 
-            " bg-transparent focus:outline-none border-b-[1px] placeholder-[#C0C0C0] w-full"} 
+            " bg-transparent focus:outline-none border-b-[1px] placeholder-[#9b9b9b] w-full"} 
         placeholder={placeholder}
         onChange={onChange}
         />
