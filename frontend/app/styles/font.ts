eaf73bd03f26b9338a04e5469ca8db65acc14e5c
catskhi <catskhi@proton.me>
2025-03-08 23:50:04 -0300

    Feat: Added more fonts to font configuration

--- frontend/app/styles/font.ts
+++ frontend/app/styles/font.ts
@@ -5,9 +5,13 @@ export const gohuFont = localFont({
 })
 
 export const toshibaSatFont = localFont({
-  src: '../../public/fonts/Ac437_ToshibaSat_8x14.ttf'
+  src: '../../public/fonts/WebPlus_ToshibaSat_8x14.woff'
 })
 
 export const webplusFont = localFont({
   src: '../../public/fonts/WebPlus_HP_100LX_6x8.woff'
+})
+
+export const ibmEgaFont = localFont({
+  src: '../../public/fonts/Web437_IBM_EGA_8x14.woff'
 })
