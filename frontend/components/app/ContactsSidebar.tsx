0028ccc08c9cf0d2cd1d15010d7d990f26a03c1b
catskhi <catskhi@proton.me>
2025-03-08 23:44:28 -0300

    feat: Added prototype of ContactsSideBar

---
+++ frontend/components/app/ContactsSidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import FormTextInput from "../forms/FormTextInput";
+import TextGlitchEffect from "../style/TextGlitchEffect";
+
+type Contact = {
+  id: string;
+  name: string;
+  status: 'online' | 'offline';
+  unread: number;
+  keyValid: boolean;
+};
+
+interface Props {
+    className?: string
+}
+
+export default function ContactsSidebar({className}: Props) {
+    return (
+        <div className={className + ' w-64 border-[#9b9b9b]'}>
+            <div className="px-4 mt-4">
+                <FormTextInput placeholder="Search... " className="border-[1px] p-2" />
+            </div>
+        </div>
+    )
+}
