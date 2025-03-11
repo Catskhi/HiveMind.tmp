d73d8b1416b87e46511891211cf1c82fe34eef5a
catskhi <catskhi@proton.me>
2025-03-10 22:53:36 -0300

    refactor: Refactored ChatMessages component to render messages on a separated ChatMessage component

--- frontend/components/app/chat/ChatMessages.tsx
+++ frontend/components/app/chat/ChatMessages.tsx
@@ -4,12 +4,14 @@ import { useChat } from "./ChatProvider"
 
 export const ChatMessages = () => {
     const { messages } = useChat()
-
     return (
-        <div className="flex-1 pt-3 overflow-y-scroll">
-            <div className="flex flex-col gap-y-2">
+        <div className="flex-1 pt-3 overflow-auto">
+            <div className="flex flex-col gap-y-3">
                 {messages.map((message, index) => (
-                    <ChatMessage key={index} username={message.username} message={message.message} />
+                    <ChatMessage key={index} 
+                    username={message.username} 
+                    message={message.message}
+                    timestamp={message.timestamp} />
                 ))}
             </div>
         </div>
