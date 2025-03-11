aec2187cc0a12310e162792c9d1b54e1d0916af6
catskhi <catskhi@proton.me>
2025-03-10 22:53:13 -0300

    feat: Added ChatMessage component

--- frontend/components/app/chat/ChatMessage.tsx
+++ frontend/components/app/chat/ChatMessage.tsx
@@ -1,13 +1,29 @@
+import { ibmEgaFont } from "@/app/styles/font"
 
 interface Props {
     username: string
     message: string
+    timestamp: string
 }
 
-export default function ChatMessage({username, message}: Props) {
+export default function ChatMessage({username, message, timestamp}: Props) {
+
+    const formatTimeStamp = (): string => {
+        const date = new Date(timestamp)
+        return date.toLocaleTimeString(navigator.language, {
+            day: '2-digit',
+            month: 'short',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+        })   
+    }
+ 
     return (
-        <div>
-            ({username}): {message}
+        <div className={ibmEgaFont.className}>
+            <span className="text-cyan-400 text-sm">[{formatTimeStamp()}] </span>
+            <span className="text-lime-300 text-sm">@{username} </span> <br/>
+            <span className="select-none">{'> '}</span><span className="text-white">{message}</span>
         </div>
     )
 }
