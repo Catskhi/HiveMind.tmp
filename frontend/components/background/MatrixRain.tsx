849198d9760a0a6dc99dd58cf3912f38c00770b1
catskhi <catskhi@proton.me>
2025-03-25 15:17:50 -0300

    refactor: Adjusted code to make the MatrixRain adapt to the screen resize

--- frontend/components/background/MatrixRain.tsx
+++ frontend/components/background/MatrixRain.tsx
@@ -3,11 +3,13 @@
 import { toshibaSatFont, webplusFont } from "@/app/styles/font";
 import { useEffect, useRef } from "react"
 
-const ascii_characters = "░▒▓█▓▒░<>[]{}()/\\|!@#$%^&*-_=+"
 const characters = 'アイウエオ0123456789<>[]{}()/\\|!@#$%^&*-_=+ΩΣΨΞДЖЙΛФ`';
 
 export default function MatrixRain() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
+    const columnsRef = useRef<number>(0)
+    const dropsRef = useRef<number[]>([])
+    const fontSize = 16
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -16,41 +18,48 @@ export default function MatrixRain() {
         const ctx = canvas.getContext('2d')
         if (!ctx) return;
 
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
+        const initializeCanvas = () => {
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+            columnsRef.current = Math.floor(canvas.width / fontSize)
+            dropsRef.current = Array(columnsRef.current).fill(0)
+        }
 
-        const fontSize = 16;
-        const columns = Math.floor(canvas.width / fontSize)
-        const drops: number[] = Array(columns).fill(0);
+        initializeCanvas()
 
         const draw = () => {
+            if (!canvas || !ctx) return
+
             ctx.fillStyle = "rgba(0, 0, 0, 0.05)"
             ctx.fillRect(0, 0, canvas.width, canvas.height)
 
             ctx.fillStyle = "rgba(192, 192, 192, 0.25)"
             ctx.font = `${fontSize}px ${toshibaSatFont.style.fontFamily}`
 
-            for(let i = 0; i < drops.length; i++) {
+            for (let i = 0; i < dropsRef.current.length; i++) {
                 const text = characters.charAt(Math.floor(Math.random() * characters.length))
-                const x_coord = i *fontSize
-                const y_coord = drops[i] * fontSize
-                ctx.fillText(text, x_coord, y_coord)
+                const x = i * fontSize
+                const y = dropsRef.current[i] * fontSize
 
-                if(drops[i]*fontSize > canvas.height && Math.random() > 0.975) {
-                    drops[i] = 0
+                ctx.fillText(text, x, y)
+                if (y > canvas.height && Math.random() > 0.975) {
+                    dropsRef.current[i] = 0
                 }
-                drops[i]++
+                dropsRef.current[i]++
             }
         }
 
         const interval = setInterval(draw, 30)
 
         const handleResize = () => {
+            if (!canvas) return
             canvas.width = window.innerWidth
             canvas.height = window.innerHeight
+            columnsRef.current = Math.floor(canvas.width / fontSize)
+            dropsRef.current = Array(columnsRef.current).fill(0)
         }
-        window.addEventListener('resize', handleResize)
 
+        window.addEventListener('resize', handleResize)
         return () => {
             clearInterval(interval)
             window.removeEventListener("resize", handleResize)
@@ -59,9 +68,7 @@ export default function MatrixRain() {
 
     return (
         <canvas ref={canvasRef} 
-        className={ webplusFont.className + 
-        " absolute top-0 left-0 w-full h-full -z-10"}>
-
-        </canvas>
+                className={`${webplusFont.className} absolute top-0 left-0 w-full h-full -z-10`}
+        />
     )
 }
