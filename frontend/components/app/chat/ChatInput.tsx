d22aa3b4256e728e060a103971870f40a3d22c51
catskhi <catskhi@proton.me>
2025-03-26 22:58:24 -0300

    fix: Removed chat test chat input

--- frontend/components/app/chat/ChatInput.tsx
+++ frontend/components/app/chat/ChatInput.tsx
@@ -2,7 +2,6 @@
 import FormTextInput from "@/components/forms/FormTextInput";
 import { useChat } from "./ChatProvider";
 import { useState } from "react";
-import { useAuth } from "../auth/AuthProvider";
 
 interface Props {
     username: string
@@ -10,12 +9,12 @@ interface Props {
 
 
 export default function ChatInput({ username }: Props) {
-    const { sendMessage, sendPrivateMessage, recipientName, setRecipientName } = useChat()
+    const { sendMessage, isConnected } = useChat()
     const [inputValue, setInputValue] = useState<string>('')
 
     const handleSend = () => {
         if (inputValue.trim()) {
-            sendPrivateMessage(inputValue.trim())
+            sendMessage(inputValue.trim())
             setInputValue('')
         }
     }
@@ -29,19 +28,13 @@ export default function ChatInput({ username }: Props) {
 
     return (
         <div className="p-4 flex items-center">
+            Is connected: {isConnected ? ' true ' : ' false '}
             ({username}@hivemind.tmp) <span className="mx-2">$</span> 
             <FormTextInput 
                 onKeyDown={handleKeyDown}
                 className="w-1/2 flex-1"
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
-            />
-            <FormTextInput 
-                onKeyDown={handleKeyDown}
-                placeholder="Recipiant Name"
-                className="w-1/2 flex-1 border-b-green-500"
-                value={recipientName}
-                onChange={(e) => setRecipientName(e.target.value)}
             />
             <button className="px-2 ml-2 self-center"
                 onClick={handleSend}
