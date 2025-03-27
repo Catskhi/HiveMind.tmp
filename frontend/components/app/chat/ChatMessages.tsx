e09c111e60659aa252bac0a05e353b6902ca31cf
catskhi <catskhi@proton.me>
2025-03-26 22:59:32 -0300

    refactor: Refactored to add global messages and private messages

--- frontend/components/app/chat/ChatMessages.tsx
+++ frontend/components/app/chat/ChatMessages.tsx
@@ -1,15 +1,39 @@
 'use client'
-import ChatMessage from "./ChatMessage"
+import { useEffect, useMemo } from "react"
 import { useChat } from "./ChatProvider"
+import GlobalChatMessage from "./GlobalChatMessage"
+import PrivateChatMessage from "./PrivateChatMessage"
 
 export const ChatMessages = () => {
-    const { messages } = useChat()
+    const { globalMessages, privateMessages, chatName, isGlobalChat } = useChat()
+
+    const messages = useMemo(() => {
+        return isGlobalChat ? globalMessages : privateMessages
+    }, [isGlobalChat, globalMessages, privateMessages])
+    
+    useEffect(() => {
+        console.log(messages)
+    }, [messages])
+
     return (
         <div className="flex-1 pt-3 overflow-auto">
+            <div className="pt-3 pb-5">
+                { chatName }, {isGlobalChat ? 'true' : 'false'}
+            </div>
             <div className="flex flex-col gap-y-3">
-                {messages.map((message, index) => (
-                    <ChatMessage key={index} 
+                
+                {isGlobalChat && messages.map((message, index) => (
+                    <GlobalChatMessage 
+                    key={index} 
                     username={message.username} 
+                    message={message.message}
+                    timestamp={message.timestamp} />
+                ))}
+                {!isGlobalChat && messages.map((message, index) => (
+                    <PrivateChatMessage 
+                    key={index} 
+                    sender={message.sender} 
+                    recipient={message.recipient}
                     message={message.message}
                     timestamp={message.timestamp} />
                 ))}
