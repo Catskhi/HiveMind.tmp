044e27ecf9aaf39b04aea437d911b7dad1532927
catskhi <catskhi@proton.me>
2025-03-26 23:04:55 -0300

    feat: Added disabled and onClick to button

--- frontend/components/ui/TextButton.tsx
+++ frontend/components/ui/TextButton.tsx
@@ -5,12 +5,16 @@ interface Props {
     title?: string
     className?: string
     type?: "submit"
+    disabled?: boolean
+    onClick?: () => void
 }
 
-export default function TextButton({children, title, className, type}: Props) {
+export default function TextButton({children, title, className, type, onClick, disabled}: Props) {
     return (
-        <button title={title} className={className + ' hover:text-[#C0C0C0] cursor-pointer '}
+        <button title={title} className={className + ' hover:text-[#C0C0C0] disabled:text-[#707070] cursor-pointer '}
             type={type}
+            onClick={onClick}
+            disabled={disabled}
         >
           {children}
         </button>
