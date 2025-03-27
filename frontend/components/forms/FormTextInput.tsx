3e405126eb95146f47d4397e2b05493bc1701bdb
catskhi <catskhi@proton.me>
2025-03-26 23:04:19 -0300

    feat: Added maxLength functionality

--- frontend/components/forms/FormTextInput.tsx
+++ frontend/components/forms/FormTextInput.tsx
@@ -5,16 +5,18 @@ interface Props {
     className?: string,
     placeholder?: string
     value?: string
+    maxLength?: number
     onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function FormTextInput({type, required, className, placeholder, onChange, onKeyDown, value}: Props) {
+export default function FormTextInput({type, required, className, placeholder, onChange, onKeyDown, value, maxLength}: Props) {
     return (
         <input 
         value={value}
         type={type ? type : "text"} required={required}
         onKeyDown={onKeyDown}
+        maxLength={maxLength}
         className={className + 
             " bg-transparent focus:outline-none border-b-[1px] placeholder-[#9b9b9b] w-full"} 
         placeholder={placeholder}
