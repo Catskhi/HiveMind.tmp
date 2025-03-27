019f8980cf94288dead84b06cb16526f8fd85efe
catskhi <catskhi@proton.me>
2025-03-26 23:06:55 -0300

    refactor: Refactored PrivateMessageRequest to add neccessary fields

--- backend/src/main/java/com/hivemind/controller/request/PrivateMessageRequest.java
+++ backend/src/main/java/com/hivemind/controller/request/PrivateMessageRequest.java
@@ -1,4 +1,4 @@
 package com.hivemind.controller.request;
 
-public record PrivateMessageRequest(String content, String recipientId) {
+public record PrivateMessageRequest(String recipient, String encryptedKeySender, String encryptedKeyRecipient, String encryptedMessage, String iv) {
 }
