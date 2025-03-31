13e013d350b306391e7b510a13df6d32a71a736a
catskhi <catskhi@proton.me>
2025-03-31 20:49:25 -0300

    refactor: Refactored the code to handle contacts instead of values directly into memory

--- frontend/components/app/ContactsSidebar.tsx
+++ frontend/components/app/ContactsSidebar.tsx
@@ -3,39 +3,29 @@
 import { useEffect } from "react";
 import FormTextInput from "../forms/FormTextInput";
 import { useChat } from "./chat/ChatProvider";
-
-type Contact = {
-  id: string;
-  name: string;
-  status: 'online' | 'offline';
-  unread: number;
-  keyValid: boolean;
-};
+import Contact from "./Contact";
 
 interface Props {
     className?: string
 }
 
-export default function ContactsSidebar({className}: Props) {
-    const { unreadMessages, recipientName } = useChat()
+export default function ContactsSidebar({className}: Props)  {
+    const { contacts, fetchContacts } = useChat()
 
     useEffect(() => {
-        console.log(unreadMessages)
-    }, [unreadMessages])
+        fetchContacts()
+    }, [])
     
     return (
         <div className={className + ' w-64 border-[#9b9b9b]'}>
-            <div className="px-4 mt-4">
+            <div className="mt-4 w-64 px-2">
                 <FormTextInput placeholder="Search... " className="border-[1px] p-2" />
             </div>
-            {unreadMessages.map((unreadMessage) => (
-                    <li key={unreadMessage.sender} className="flex justify-between p-2 border-b border-gray-300">
-                        <span>{unreadMessage.sender}</span>
-                        <span className="bg-red-500 text-white px-2 py-1 rounded-full text-xs">
-                            {unreadMessage.count}
-                        </span>
-                    </li>
-                ))}
+            <div className="px-2">
+                {contacts && contacts.map((contact) => (
+                        <Contact key={contact.contact.name} sender={contact.contact.name} count={contact.unreadMessagesCount} />
+                    ))}
+            </div>
         </div>
     )
 }
