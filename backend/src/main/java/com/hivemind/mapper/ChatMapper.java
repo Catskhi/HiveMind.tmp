92d9fba8b9816c22e7b12a106f68a04cb50a5936
catskhi <catskhi@proton.me>
2025-04-03 21:27:12 -0300

    feat: Added globalMessages mapping

--- backend/src/main/java/com/hivemind/mapper/ChatMapper.java
+++ backend/src/main/java/com/hivemind/mapper/ChatMapper.java
@@ -1,7 +1,10 @@
 package com.hivemind.mapper;
 
+import com.hivemind.controller.request.GlobalMessageRequest;
 import com.hivemind.controller.request.PrivateMessageRequest;
+import com.hivemind.controller.response.GlobalMessageResponse;
 import com.hivemind.controller.response.PrivateMessageResponse;
+import com.hivemind.entity.GlobalMessage;
 import com.hivemind.entity.PrivateMessage;
 import com.hivemind.entity.User;
 
@@ -9,7 +12,25 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ChatMapper {
+
+
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public static GlobalMessage toGlobalMessage(GlobalMessageRequest request, User sender, ZonedDateTime createdAt) {
+        return GlobalMessage.builder()
+                .sender(sender)
+                .encryptedMessage(request.message())
+                .createdAt(createdAt)
+                .build();
+    }
+
+    public static GlobalMessageResponse toGlobalMessageResponse(GlobalMessage globalMessage) {
+        return GlobalMessageResponse.builder()
+                .username(globalMessage.getSender().getName())
+                .message(globalMessage.getEncryptedMessage())
+                .timestamp(globalMessage.getCreatedAt().format(FORMATTER))
+                .build();
+    }
 
     public static PrivateMessage toPrivateMessage(PrivateMessageRequest request, User sender, User recipient, ZonedDateTime createdAt) {
         return PrivateMessage.builder()
